31e848ac0a6b1e3a0f0474651fb54dbb43692f10
Michael Smith <michael.smith@puppetlabs.com>
2015-03-17 11:22:15 -0700

    Add Gmvault beta package
    Adds a Gmvault package. Gmvault only exists as a beta release for now;
    once it has a final release a new line will be created for the main
    releases.

---
+++ manual/gmvault.beta/tools/chocolateyInstall.ps1
@@ -0,0 +1,5 @@
+﻿$packageId = 'gmvault'
+$silentArgs = "/S"
+$url = "https://bitbucket.org/gaubert/gmvault-official-download/downloads/gmvault_installer_v1.8.1-beta.exe"
+
+Install-ChocolateyPackage "$packageId" 'exe' "$silentArgs" "$url"
